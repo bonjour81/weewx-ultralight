3ae6fde976e9396d6e13ba0419dcca3965415ee6
bonjour81 <bonjour81@rbox.me>
2020-05-08 22:24:54 +0200

    typo correction

--- install.py
+++ install.py
@@ -8,31 +8,30 @@ def loader():
 
 class ultralightInstaller(ExtensionInstaller):
     def __init__(self):
-    super(ultralightInstaller, self).__init__(
-        version = "1.0",
-        name = 'ultralight',
-        description = 'An ultra light weight skin',
-        author = "F.Stuyk",
-        author_email = "bonjour81@runbox.com",
-        config = {
-            'StdReport': {
-                'ultralight': {
-                    'skin': 'ultralight',
-                    'HTML_ROOT': 'ultra'
+        super(ultralightInstaller, self).__init__(
+            version = "1.0",
+            name = 'ultralight',
+            description = 'An ultra light weight skin',
+            author = "F.Stuyk",
+            author_email = "bonjour81@runbox.com",
+            config = {
+                'StdReport': {
+                    'ultralight': {
+                        'skin': 'ultralight',
+                        'HTML_ROOT': 'ultra'
+                    }
                 }
-            }
-        },
-        files = [('skins/ultralight', ['skins/ultralight/index.html.tmpl',
-                'skins/ultralight/skin.conf'
-            ]),
-            ('skins/ultralight/js', ['skins/ultralight/js/collapsible.js']),
-            ('skins/ultralight/css', ['skins/ultralight/css/mobile.css']),
-            ('skins/ultralight/images', ['skins/ultralight/images/arrow_down.gif',
+            },
+            files = [('skins/ultralight', ['skins/ultralight/index.html.tmpl',
+                    'skins/ultralight/skin.conf'
+                ]),
+                ('skins/ultralight/js', ['skins/ultralight/js/collapsible.js']),
+                ('skins/ultralight/css', ['skins/ultralight/css/mobile.css']),
+                ('skins/ultralight/images', ['skins/ultralight/images/arrow_down.gif',
                 'skins/ultralight/images/arrow_up.gif',
                 'skins/ultralight/images/b.gif',
                 'skins/ultralight/images/nav-current.jpg',
                 'skins/ultralight/images/nav.jpg'
-            ])
-            
-        ]
-    )
+                ])
+            ]
+        )
