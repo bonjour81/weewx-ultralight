7e5ec3cc95cc2d39d20438c99ccb2d385e2298d2
bonjour81 <bonjour81@rbox.me>
2020-05-08 22:44:24 +0200

    corrections

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 # Installer four ultralight weewx skin
 # F.Stuyk, 2020
 
-from setup import ExtensionInstaller
+from weecfg.extension import ExtensionInstaller
 
 def loader():
     return ultralightInstaller()
@@ -14,24 +14,23 @@ class ultralightInstaller(ExtensionInstaller):
             description = 'An ultra light weight skin',
             author = "F.Stuyk",
             author_email = "bonjour81@runbox.com",
-            config = {
+            config={
                 'StdReport': {
                     'ultralight': {
-                        'skin': 'ultralight',
-                        'HTML_ROOT': 'ultra'
-                    }
-                }
-            },
-            files = [('skins/ultralight', ['skins/ultralight/index.html.tmpl',
-                    'skins/ultralight/skin.conf'
-                ]),
-                ('skins/ultralight/js', ['skins/ultralight/js/collapsible.js']),
-                ('skins/ultralight/css', ['skins/ultralight/css/mobile.css']),
-                ('skins/ultralight/images', ['skins/ultralight/images/arrow_down.gif',
-                'skins/ultralight/images/arrow_up.gif',
-                'skins/ultralight/images/b.gif',
-                'skins/ultralight/images/nav-current.jpg',
-                'skins/ultralight/images/nav.jpg'
-                ])
-            ]
-        )
+                        'skin':'ultralight',
+                        'HTML_ROOT':'ultra'}}},
+            files=[('skins/ultralight',
+                    ['skins/ultralight/index.html.tmpl',
+                    'skins/ultralight/skin.conf']),
+                   ('skins/ultralight/js',
+                    ['skins/ultralight/js/collapsible.js']),
+                   ('skins/ultralight/css',
+                    ['skins/ultralight/css/mobile.css']),
+                   ('skins/ultralight/images',
+                    ['skins/ultralight/images/arrow_down.gif',
+                     'skins/ultralight/images/arrow_up.gif',
+                     'skins/ultralight/images/b.gif',
+                     'skins/ultralight/images/nav-current.jpg',
+                     'skins/ultralight/images/nav.jpg'])
+                   ]
+            )  
